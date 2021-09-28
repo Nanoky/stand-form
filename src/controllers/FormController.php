c1e22dab28f325aeea40e325483e74c2a2f75182
Elsa Zougouri <konancharlesyao@gmail.com>
2021-09-28 18:58:50 +0000

    returned data set

--- src/controllers/FormController.php
+++ src/controllers/FormController.php
@@ -26,28 +26,34 @@ class FormController extends ControllerInterface
             $exposant = new Exposant(["id" => -1, "type_id" => $data["type_id"], "name" => $data["name"], "firstname" => $data["firstname"], "address" => $data["address"], "city" => $data["city"], "country" => $data["country"], "phone" => $data["phone"], "celphone" => $data["celphone"], "email" => $data["email"], "url" => $data["url"]]);
             $exposant->save();
 
-            $fields = $data["field_id"];
-            foreach ($fields as $key => $value) {
+            $fields_data = $data["field_id"];
+            $fields = [];
+            foreach ($fields_data as $key => $value) {
                 $field_exposant = new ExposantField(["exposant_id" => $exposant->id, "field_id" => $value]);
                 $field_exposant->save();
+                $fields[] = $field_exposant;
             }
 
             $title = $data["title_id"];
+            $titles = [];
             foreach ($title as $key => $value) {
                 $title_exposant = new ExposantTitle(["id" => $exposant->id, "title_id" => $value]);
                 $title_exposant->save();
+                $titles[] = $title_exposant;
             }
 
-            $stands = $data["stand_id"];
-            foreach ($stands as $key => $value) {
+            $stands_data = $data["stand_id"];
+            $stands = [];
+            foreach ($stands_data as $key => $value) {
                 $exposant_stand = new ExposantStand(["id_exposant" => $exposant->id, "id_stand" => $value->id]);
                 $exposant_stand->save();
+                $stands[] = $exposant_stand;
             }
 
             $payment = new Payment(["id" => -1, "exposant_id" => $exposant->id, "payment_mode_id" => $data["payment_mode_id"], "montant" => $data["montant"], "date" => $data["date"]]);
             $payment->save();
 
-            return $this->respond(true, "Exposant enregistrer", $exposant);
+            return $this->respond(true, "Exposant enregistrer", ["exposant" => $exposant, "payment" => $payment, "titles" => $titles, "fields" => $fields, "stands" => $stands]);
         }
 
         public function update(Request $request)
